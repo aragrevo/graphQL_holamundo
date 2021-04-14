e27fb46a023488c9e990caaa7baeeebfb8e97f6a
aragrevo <eduver_san@hotmail.com>
2021-04-14 12:02:28 -0500

    refactor archivos

--- src/server.ts
+++ src/server.ts
@@ -1,44 +1,13 @@
 import express from 'express';
 var cors = require('cors')
 import compression from 'compression';
-import { IResolvers } from '@graphql-tools/utils';
-import { GraphQLSchema } from 'graphql';
-import { makeExecutableSchema } from '@graphql-tools/schema';
 import { graphqlHTTP } from 'express-graphql';
+import schema from "./schema";
 
 
 const app = express();
 app.use('*', cors());
 app.use(compression());
-
-
-// Types GraphQL
-const typeDefs = `
-    type Query {
-        hola: String!
-        holaConNombre(nombre: String!): String!
-        holaAlCursoGraphQL: String!
-    }
-`
-
-const resolvers: IResolvers = {
-    Query: {
-        hola(): string {
-            return 'Hola Mundo';
-        },
-        holaConNombre(__: void, { nombre }): string {
-            return `Hola Mundo ${nombre}`;
-        },
-        holaAlCursoGraphQL(): string {
-            return 'Hola Mundo en el curso GraphQL';
-        }
-    }
-}
-
-const schema: GraphQLSchema = makeExecutableSchema({
-    typeDefs,
-    resolvers
-})
 
 app.use('/', graphqlHTTP({
     schema,
